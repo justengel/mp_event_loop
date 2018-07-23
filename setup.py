a682fffbdac45117b5bdb2a250280760d6d650c6
justengel <jtengel08@gmail.com>
2018-07-23 09:48:31 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -32,10 +32,10 @@ if __name__ == "__main__":
 
     setup(
         name="mp_event_loop",
-        version="1.2.5",
+        version="1.3.0",
         description="Library for long running multiprocessing event loops.",
         url="https://github.com/justengel/mp_event_loop",
-        download_url="https://github.com/justengel/mp_event_loop/archive/v1.2.5.tar.gz",
+        download_url="https://github.com/justengel/mp_event_loop/archive/v1.3.0.tar.gz",
 
         keywords=["multiprocessing", "event", "loop", "event loop", "process"],
 
